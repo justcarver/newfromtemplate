9995a5804c616925fc6864c73f819fa8cae18d08
Justin Carver <carver@justcarver.com>
2017-03-20 11:47:25 -0400

    Initial Package Generated

---
+++ spec/newfromtemplate-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NewfromtemplateView from '../lib/newfromtemplate-view';
+
+describe('NewfromtemplateView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
